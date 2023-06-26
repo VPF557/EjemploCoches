dff8c00b2e9a7737efb34edb514e3ecd5bcc19ac
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-06-26 19:48:52 +0200

    Importada clase DataHandling

---
+++ Backend/src/main/java/EjemploCoches/example/EjemploCoches/DataHanding.java
@@ -0,0 +1,60 @@
+package EjemploCoches.example.EjemploCoches;
+
+import java.util.ArrayList;
+
+public class DataHanding {
+    public ArrayList<Criptomoneda> AnadirObjeto1(Criptomoneda objeto1, String ruta)
+    {
+        //En esta funcion se añaden los nuevos elementos enviados desde el front a un ArrayList para posteriormente actualizar la BBDD
+        LeerJson reader = new LeerJson();
+        ArrayList<Criptomoneda> listaAux = reader.LeerFicheroJson1(ruta);
+        objeto1.setId(listaAux.get(listaAux.size() - 1).getId() + 1);
+        listaAux.add(objeto1);
+        return listaAux;
+    }
+
+    public ArrayList<Criptomoneda> EliminarObjeto (int id, String ruta)
+    {
+        //En esta funcion se añaden los nuevos elementos enviados desde el front a un ArrayList para posteriormente actualizar la BBDD
+        LeerJson reader = new LeerJson();
+        ArrayList<Criptomoneda> listaAux = reader.LeerFicheroJson1(ruta);
+        for (int i = 0; i < listaAux.size() ; i++ )
+        {
+            if(listaAux.get(i).getId() == id)
+            {
+                listaAux.remove(i);
+            }
+        }
+        return listaAux;
+    }
+
+    public int ComprobarObjeto(Criptomoneda objeto1, String ruta , ArrayList<Criptomoneda> listaAux)
+    {
+        //En esta funcion se añaden los nuevos elementos enviados desde el front a un ArrayList para posteriormente actualizar la BBDD
+        LeerJson reader = new LeerJson();
+        int control = 0;
+
+        for (int i = 0; i < listaAux.size(); i++)
+        {
+            if
+            (       objeto1.getNombre().equals(listaAux.get(i).getNombre()) &&
+                    objeto1.getValor() ==  listaAux.get(i).getValor() &&
+                    objeto1.getFluctuacion() == listaAux.get(i).getFluctuacion() &&
+                    objeto1.getCategoria().equals(listaAux.get(i).getCategoria()) &&
+                    objeto1.getPopularidad().equals(listaAux.get(i).getPopularidad())
+            )
+            {
+                control = 1;
+            }
+            else
+            {
+                control = 0;
+            }
+
+        }
+        return control;
+    }
+
+
+
+}
