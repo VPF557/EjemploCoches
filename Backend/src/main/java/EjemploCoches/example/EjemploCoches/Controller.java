38a7f467eba63f2191e59bdaece614cc6ac06005
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-06-26 20:04:55 +0200

    Arreglado peticion delete en controller

--- Backend/src/main/java/EjemploCoches/example/EjemploCoches/Controller.java
+++ Backend/src/main/java/EjemploCoches/example/EjemploCoches/Controller.java
@@ -27,16 +27,16 @@ public class Controller {
         escribirJSON.escribirObjeto1(lista);
         return lista;
     }
-/*
-    @DeleteMapping("/CriptoB{id}")
-    public ArrayList<Criptomoneda> Borrar(@RequestParam int id)
+
+    @DeleteMapping("/CochesA{id}")
+    public ArrayList<Coche> Borrar(@RequestParam int id)
     {
         DataHanding dataHanding = new DataHanding();
-        ArrayList<Criptomoneda> lista;
-        lista = dataHanding.EliminarObjeto(id,"Criptomonedas.json");
+        ArrayList<Coche> lista;
+        lista = dataHanding.EliminarObjeto(id,"Almacen.json");
         EscribirJson escribirJSON = new EscribirJson();
         escribirJSON.escribirObjeto1(lista);
         return lista;
     }
-*/
+
 }
