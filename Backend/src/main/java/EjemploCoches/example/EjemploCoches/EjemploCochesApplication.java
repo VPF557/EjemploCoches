da00c6b705a1b43cda38a4f9e15c2f0395d5a289
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-06-26 19:32:04 +0200

    Back añadido desde Spring

---
+++ Backend/src/main/java/EjemploCoches/example/EjemploCoches/EjemploCochesApplication.java
@@ -0,0 +1,13 @@
+package EjemploCoches.example.EjemploCoches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploCochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploCochesApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/EjemploCoches/example/EjemploCoches/EjemploCochesApplicationTests.java
@@ -0,0 +1,13 @@
+package EjemploCoches.example.EjemploCoches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploCochesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
